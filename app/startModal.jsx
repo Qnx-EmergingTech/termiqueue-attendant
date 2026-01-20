1f0c606bb40aa7731a7af82599f375644dc77feb
unknown <john.edward_complido@questronix.com.ph>
2026-01-20 12:48:30 +0800

    [FRONTEND]Feat: Seats Integration in Start Modal

--- app/startModal.jsx
+++ app/startModal.jsx
@@ -1,14 +1,43 @@
 import { useLocalSearchParams, useRouter } from 'expo-router';
-import { useState } from "react";
-import { departBus } from "../api/buses";
+import { useEffect, useState } from "react";
+import { Alert } from "react-native";
+import { departBus, getAttendantPassengers } from "../api/buses";
 import CustomizableModal from "../app/common/commonModal";
 import { setTripState } from "../utils/authStorage";
 
-export default function startModal() {
+export default function StartModal() {
   const router = useRouter();
   const { busId } = useLocalSearchParams();
   const [visible, setVisible] = useState(true);
   const [loading, setLoading] = useState(false);
+
+  // Dynamic passenger info
+  const [capacity, setCapacity] = useState(0);
+  const [seatTaken, setSeatTaken] = useState(0);
+  const [fetchingSeats, setFetchingSeats] = useState(true);
+
+  // Fetch current passengers and capacity
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const result = await getAttendantPassengers();
+        if (!result.success) {
+          Alert.alert("Error", "Failed to fetch passenger information");
+          return;
+        }
+
+        setCapacity(result.capacity);
+        const boarded = result.passengers.filter(p => p.status === "boarded");
+        setSeatTaken(boarded.length);
+      } catch (err) {
+        Alert.alert("Error", "Something went wrong");
+      } finally {
+        setFetchingSeats(false);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   const handleConfirm = async () => {
     if (!busId) {
@@ -16,17 +45,17 @@ export default function startModal() {
       return;
     }
     setLoading(true);
+
     const result = await departBus(busId);
 
     if (result.success) {
-      console.log("Bus departed");
-
       await setTripState("ongoing", "Finish Trip");
       setVisible(false);
       router.replace("/(tabs)/home");
     } else {
       alert(result.message);
     }
+
     setLoading(false);
   };
 
@@ -35,6 +64,8 @@ export default function startModal() {
     router.replace("/(tabs)/home");
   };
 
+  if (fetchingSeats) return null; 
+
   return (
     <CustomizableModal
       visible={visible}
@@ -42,9 +73,8 @@ export default function startModal() {
       onCancel={closeAndGoHome}
       onConfirm={handleConfirm}
       confirmDisabled={loading}
-
       title="Want to start your trip?"
-      message="Capacity is at 45/45 (Full)\nDrive Safe!"
+      message={`Capacity is at ${seatTaken}/${capacity}\nDrive Safe!`}
       confirmText={loading ? "Starting..." : "Confirm"}
       cancelText="Cancel"
       icon={require('../assets/images/success.png')}
