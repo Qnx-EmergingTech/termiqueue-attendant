e3c414dd6d1d3a20d6c4d9258e8422cd58238d29
John Edward Complido <john.edward_complido@questronix.com.ph>
2026-06-08 17:18:37 +0800

    [FRONTEND] Fix: Change and move Firebase and Google Maps Api to env variables

---
+++ app.config.js
@@ -0,0 +1,22 @@
+const { withAndroid } = require("@expo/config-plugins");
+
+/** @type {import('expo/config').ExpoConfig} */
+module.exports = ({ config }) => ({
+  ...config,
+  ios: {
+    ...config.ios,
+    config: {
+      ...config.ios?.config,
+      googleMapsApiKey: process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY,
+    },
+  },
+  android: {
+    ...config.android,
+    config: {
+      ...config.android?.config,
+      googleMaps: {
+        apiKey: process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY,
+      },
+    },
+  },
+});
